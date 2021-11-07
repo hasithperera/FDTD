c555ff876b471c1e6b94475744729399c21dfe41
Hasith Perera <aep00010@mix.wvu.edu>
2021-11-07 18:12:14 -0500

    larger 1D sim

--- src/FDTD_1D.py
+++ src/FDTD_1D.py
@@ -3,12 +3,14 @@ PHYS 611
 
 Hasith Perera
 """
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from matplotlib.animation import FuncAnimation
 
-domain_x =500;
+domain_x =5000;
 
 Ez = np.zeros([domain_x,1])
 Hy = np.zeros([domain_x,1])
@@ -19,13 +21,13 @@ Jz = np.zeros([domain_x,1])
 epsilon_r = np.ones([domain_x,1])
 
 #define medium
-epsilon_r[200:300] = 4
+epsilon_r[2000:3000] = 4
 
 
 #source location
 src_loc = 1;
 
-time_end = 1000;
+time_end = 5000;
 dt = 1
 dx = 1
 mu_0 = 1
@@ -35,13 +37,17 @@ epsilon_0 = 1
 gauss_sigma = .01;
 gauss_mu = 100
 
-
-detector = []
+detector_log = []
 
 detector_locs = [100,250,350]
+
+#define realtime animation
+animation = 0
  
 
 def src_gauss(time,mu,sigma):
+    '''Generate a gaussian source'''
+    
     return np.exp(-sigma*np.power(time-mu,2))
 
 def src_step(time,start_t,width):
@@ -53,12 +59,20 @@ def src_step(time,start_t,width):
     return 0
     
 def src_sin(time,A,f):
-    if (2*f*time > 2*np.pi):
-        return 0
+    '''src_sin(time,A,F)
+    '''
+    
+    # #sudden cut off gives discontinous edges
+    # if (2*f*time > 2*np.pi):
+    #     return 0
+    
     return A*np.sin(2*f*time)
 
 
 def dump_detector_data(Ez,time,detectors):
+    
+    detector_log.append(Ez[detectors])
+    print(Ez[detectors])
     
     print("{},".format(time),end='')
     for detector in detectors:   
@@ -82,45 +96,38 @@ def write_to_file(Ez,time,detectors,file='log.dat'):
     
 def FDTD_update(time):
     
-    #inject a source to Ez[0]      
-    #Ez[100] = np.exp(-gauss_sigma*np.power(i-gauss_mu,2))
+    # #inject a source to Ez[0]      
+    # Ez[100] = np.exp(-gauss_sigma*np.power(i-gauss_mu,2))
     
-    #gaussian source
+    # #gaussian source
     # Jz[src_loc] = src_gauss(time, gauss_mu, gauss_sigma)
     
-    
     #sine wave
-    Jz[src_loc] = src_sin(time, 1, .02)
-
-    
+    Jz[src_loc] = src_sin(time, 1, .01)
     
     #probe values
-    dump_detector_data(Ez,time,detector_locs)
+    # dump_detector_data(Ez,time,detector_locs)
 
-    
-    #update faraday's law
-    Hy[0:len(Hy)-1] = Hy[0:len(Hy)-1]+(dt/dx*mu_0)*Ez[1:len(Ez)]-Ez[0:len(Ez)-1]
-    
-    
-    
-    
-    #PBC for the Left 
-    Ez[0]=Ez[1]
-    
     #update eq from ampher's law
     Ez[1:len(Ez)] = Ez[1:len(Ez)]+(dt/(dx*epsilon_0*epsilon_r[1:len(Ez)])) * (Hy[1:len(Hy)]-Hy[0:len(Hy)-1])
     
     #current term
     Ez[1:len(Ez)] = Ez[1:len(Ez)] + dt/(epsilon_0*epsilon_r[1:len(Ez)]) * Jz[1:len(Ez)]
-    
+
     #PBC to the right
     Hy[len(Hy)-1]=Hy[len(Hy)-2]
+ 
+    #update faraday's law
+    Hy[0:len(Hy)-1] = Hy[0:len(Hy)-1]+(dt/dx*mu_0)*Ez[1:len(Ez)]-Ez[0:len(Ez)-1]    
+        
+    #PBC for the Left 
+    Ez[0]=Ez[1]   
+    print(time)
     
     
-def ftdt_animate(time):
+def fdtd_animate(time):
     
     FDTD_update(time)
-    
     ln.set_data(np.arange(0,domain_x),Ez)
     return ln,
 
@@ -132,30 +139,28 @@ def init():
     print("Clear Ez")
     return ln,
 
-
-
-#define realtime animation
-animation = 0
-
 if __name__=='__main__':
     
     
-    print("Start") 
-    
-    
-    
+    print("Start FDTD 1D simulation")     
     if animation:
+        print("With Realtime animation")
         fig, ax = plt.subplots()
         xdata, ydata = [], []
-        ln, = plt.plot([], [], 'ro')
+        ln, = plt.plot([], [], 'r.')
     
-        ani = FuncAnimation(fig, FDTD_update, frames=np.linspace(1, time_end, time_end),
+        ani = FuncAnimation(fig, fdtd_animate, frames=np.linspace(1, time_end, time_end),
                     init_func=init, blit=True,interval=10,repeat=False)
     
         plt.show()
         print("Exit")
     else:
-        
         for i in range(0,time_end):
             FDTD_update(i)
+            
+    
+    # log = np.array(detector_log)
+    # plt.plot(log[:,0])
+    # plt.plot(log[:,1])
+    # plt.plot(log[:,2])
     
